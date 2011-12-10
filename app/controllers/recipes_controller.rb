0574846094169854c5d03b2710a157f755ab91fd
Andy Camp <andy@kingofweb.com>
2011-12-10 15:36:25 -0800

    forgot a file

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,7 +4,11 @@ class RecipesController < ApplicationController
   end
 
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.limit(100)
+    @recipes = @recipes.where("name like '%#{params[:recipe_name]}%'") if params[:recipe_name]
+    if params[:ingredient_name]
+      @recipes = Recipe.joins(:ingredients).where("ingredients.name like '%#{params[:ingredient_name]}%'")
+    end
   end
 
 end
