ec13dc6ee3cd61d905c2b34b06ff424c4da27d5e
Andy Camp <andy@kingofweb.com>
2011-12-11 21:51:50 -0800

    add some basic heading to index pages so you know where you are

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,16 +4,19 @@ class RecipesController < ApplicationController
   end
 
   def index
+    @subheader = "All Recipes"
     @recipes = Recipe.joins(:ingredients).select('distinct(recipes.id), recipes.*').where("ingredients.name like '%#{params[:ingredient_name]}%'")
     #@recipes = Recipe.all
   end
 
   def recipe_search
+    @subheader = "Recipes with '#{params[:q]}' in their name"
     @recipes = Recipe.where("name like '%#{params[:q]}%'")
     render :index
   end
 
   def ingredient_search
+    @subheader = "Recipes with '#{params[:q]}' in an ingredient name"
     @recipes = Recipe.joins(:ingredients).select('distinct(recipes.id), recipes.*').where("ingredients.name like '%#{params[:q]}%'")
     render :index
   end
